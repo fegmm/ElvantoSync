ea5ec8d2bec1b8baa83208366ae70e90f5a28f19
David Schnurr <bmg1881@gmail.com>
2025-07-22 20:57:20 +0200

    adjust in groupfinder to be pulled from docker env.

--- ElvantoSync/Extensions/DependencyInjections.cs
+++ ElvantoSync/Extensions/DependencyInjections.cs
@@ -29,7 +29,7 @@ public static class DependencyInjections
         .AddTransient<ISync, GroupsToTalkSync>()
         .AddTransient<ISync, GroupsToEmailSync>();
 
-    public static IServiceCollection AddApplicationOptions(this IServiceCollection services, string username, string password)
+    public static IServiceCollection AddApplicationOptions(this IServiceCollection services, string username, string password, string nextcloudUrl)
     {
         services.AddOptions<ApplicationSettings>()
             .BindConfiguration(ApplicationSettings.ConfigSection);
@@ -66,6 +66,7 @@ public static class DependencyInjections
 
         services.AddHttpClient<IGroupFinderService, GroupFinderService>(i =>
         {
+            i.BaseAddress = new Uri(nextcloudUrl);
             i.Timeout = TimeSpan.FromMinutes(5);
             i.DefaultRequestHeaders.Authorization = auth;
             i.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
