2045b9e0c1f0dfb21fbddde907c4d91da033df00
David Schnurr <bmg1881@gmail.com>
2025-11-29 17:10:34 +0100

    deleteAdditionalGroups in groupfinder

--- ElvantoSync/Application/Groupfinder/Service/IGroupfinderService.cs
+++ ElvantoSync/Application/Groupfinder/Service/IGroupfinderService.cs
@@ -3,8 +3,10 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace ElvantoSync.GroupFinder.Service;
+
 public interface IGroupFinderService
 {
     public Task CreateGroupAsync(CreateGroupRequest request, CancellationToken cancellationToken = default);
     public Task<string[]> GetGroupAsync(CancellationToken cancellationToken = default);
+    public Task DeleteGroupAsync(string groupId, CancellationToken cancellationToken = default);
 }
